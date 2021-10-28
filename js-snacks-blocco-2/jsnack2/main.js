ddacb06e43e4ee4211577b2cd803cff6aa067cbf
pierfrancescoscar <scarinci401@gmail.com>
2021-10-28 15:35:53 +0200

    add general setup with name-surname generator

---
+++ js-snacks-blocco-2/jsnack2/main.js
@@ -0,0 +1,7 @@
+// JSnack 2
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati
+
+// Refs
+
+const nameList = ['Pierfrancesco', 'Pietro', 'Piero', 'Francesco'];
+const surnameList = ['Scarinci', 'Scarnici', 'Scaringi', 'Scarci'];
