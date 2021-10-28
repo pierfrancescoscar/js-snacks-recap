8edcaf9da91dffb6148392cab1728d196348147d
pierfrancescoscar <scarinci401@gmail.com>
2021-10-28 16:10:13 +0200

    add const for random name and random surname

--- js-snacks-blocco-2/jsnack2/main.js
+++ js-snacks-blocco-2/jsnack2/main.js
@@ -5,3 +5,14 @@
 
 const nameList = ['Pierfrancesco', 'Pietro', 'Piero', 'Francesco'];
 const surnameList = ['Scarinci', 'Scarnici', 'Scaringi', 'Scarci'];
+console.log(nameList);
+console.log(surnameList);
+
+// Generate a list of 3 people from name list and surname list
+
+const randomName = nameList[Math.floor(Math.random()*nameList.length)];
+console.log(randomName);
+
+const randomSurname = surnameList[Math.floor(Math.random()*surnameList.length)];
+console.log(randomSurname);
+
