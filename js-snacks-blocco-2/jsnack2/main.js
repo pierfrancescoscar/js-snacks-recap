27c70381c103a8197408408b882393795f856c69
pierfrancescoscar <scarinci401@gmail.com>
2021-10-28 16:25:07 +0200

    add complete "for" cycle 3 times with random names

--- js-snacks-blocco-2/jsnack2/main.js
+++ js-snacks-blocco-2/jsnack2/main.js
@@ -5,14 +5,22 @@
 
 const nameList = ['Pierfrancesco', 'Pietro', 'Piero', 'Francesco'];
 const surnameList = ['Scarinci', 'Scarnici', 'Scaringi', 'Scarci'];
-console.log(nameList);
-console.log(surnameList);
+let invitedPeopleList = '';
+// console.log(nameList);
+// console.log(surnameList);
 
 // Generate a list of 3 people from name list and surname list
 
-const randomName = nameList[Math.floor(Math.random()*nameList.length)];
-console.log(randomName);
+for (let i=0; i < 3; i++) {
 
-const randomSurname = surnameList[Math.floor(Math.random()*surnameList.length)];
-console.log(randomSurname);
+    const randomName = nameList[Math.floor(Math.random()*nameList.length)];
+    // console.log(randomName);
+
+    const randomSurname = surnameList[Math.floor(Math.random()*surnameList.length)];
+    // console.log(randomSurname);
+
+    invitedPeopleList = (randomName + ' ' + randomSurname);
+    // console.log(invitedPeopleList);
+
+}
 
